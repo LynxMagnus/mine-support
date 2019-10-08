818eacf019dbb76eb2dddf2e2755567b8081dcc6
John Watson <john.watson2@rpa.gov.uk>
2019-10-08 15:13:41 +0100

    Unique ID added to mock claim

--- server/services/api-gateway.js
+++ server/services/api-gateway.js
@@ -1,22 +1,21 @@
 const config = require('../config')
 const restClient = require('./rest-client')
-const sessionHandler = require('./session-handler')
 
 module.exports = {
   submit: async (request) => {
     try {
-      const claim = sessionHandler.get(request, 'claim')
       console.log('submitting claim')
 
       // Temporary workaround to provide valid claim to API gateway following removal of Redis
       const tempClaim = {
-        'claimId': 'MINE123',
+        'claimId': request.payload.claimId,
         'propertyType': 'business',
         'accessible': false,
         'dateOfSubsidence': '2019-07-26T09:54:19.622Z',
         'mineType': ['gold'],
-        'email': claim.email
+        'email': request.payload.email
       }
+      console.log(tempClaim)
 
       await restClient.postJson(`${config.apiGateway}/claim`, { payload: tempClaim })
       return true
