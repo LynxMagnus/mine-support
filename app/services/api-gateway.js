cfcd4e11613c1023424fd4321e0e4d5e5a137777
Paul Andrews <paul.andrews@defra.gov.uk>
2020-04-21 18:02:58 +0100

    hook up message queue

--- app/services/api-gateway.js
+++ app/services/api-gateway.js
@@ -1,6 +1,7 @@
 const config = require('../config')
 const restClient = require('./rest-client')
 const sessionHandler = require('./session-handler')
+const messageService = require('./message-service')
 
 module.exports = {
   submit: async (request) => {
@@ -8,6 +9,7 @@ module.exports = {
       const claim = sessionHandler.get(request, 'claim')
       console.log('submitting claim')
       console.log(claim)
+      await messageService.publishClaim(claim)
       await restClient.postJson(`${config.apiGateway}/claim`, { payload: claim })
       return true
     } catch (err) {
