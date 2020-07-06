b458462ba31cd7b1b1ff2f567794b4962a73191f
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-06 16:22:40 +0100

    feat: add settings for easier debugging

--- app/services/app-insights.js
+++ app/services/app-insights.js
@@ -2,7 +2,13 @@ const appInsights = require('applicationinsights')
 
 function setup () {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-    appInsights.setup().start()
+    appInsights
+      .setup()
+      // 'developer' mode
+      .setInternalLogging(true, true)
+      .start()
+    // send events more often
+    appInsights.defaultClient.config.maxBatchSize = 1
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
     appInsights.defaultClient.context.tags[cloudRoleTag] = appName
