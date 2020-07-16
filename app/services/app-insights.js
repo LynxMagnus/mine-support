1e2e8be97fbb57501c8e13a59418816a587962c9
Mark Harrop <mark.harrop@defra.gov.uk>
2020-07-16 11:06:37 +0100

    add W3C tracing headers

--- app/services/app-insights.js
+++ app/services/app-insights.js
@@ -2,7 +2,9 @@ const appInsights = require('applicationinsights')
 
 function setup () {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-    appInsights.setup().start()
+    appInsights.setup()
+      .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+      .start()
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
     appInsights.defaultClient.context.tags[cloudRoleTag] = appName
