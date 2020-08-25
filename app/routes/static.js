064e5f504501bb61cfa86527ca9298345c9ba653
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-08-25 11:49:43 +0100

    fix: correctly specify path

--- app/routes/static.js
+++ app/routes/static.js
@@ -14,22 +14,6 @@ module.exports = [{
   }
 }, {
   method: 'GET',
-  path: '/assets/{path*}',
-  options: {
-    handler: {
-      directory: {
-        path: [
-          'node_modules/govuk-frontend/govuk/assets'
-        ]
-      }
-    },
-    cache: {
-      expiresIn: config.staticCacheTimeoutMillis,
-      privacy: 'private'
-    }
-  }
-}, {
-  method: 'GET',
   path: '/static/{path*}',
   options: {
     handler: {
