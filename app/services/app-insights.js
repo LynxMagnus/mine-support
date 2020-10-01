cdfe6eb02ec989c4bf49db258ed066699204028d
fareedmohammed <fareed.mohammed.uk@gmail.com>
2020-10-01 17:01:47 +0100

    feat: add code to log if app insights key is present or not

--- app/services/app-insights.js
+++ app/services/app-insights.js
@@ -2,10 +2,13 @@ const appInsights = require('applicationinsights')
 
 function setup () {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    console.log('APPINSIGHTS_INSTRUMENTATIONKEY successfully retrieved!')
     appInsights.setup().start()
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
     appInsights.defaultClient.context.tags[cloudRoleTag] = appName
+  } else {
+    console.log('APPINSIGHTS_INSTRUMENTATIONKEY not found!')
   }
 }
 
