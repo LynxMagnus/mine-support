bfde226c812099cc742d38d1377ea554751a7163
markysoft <markharrop@yahoo.com>
2021-01-15 11:01:02 +0000

    add missing view remove comment

--- app/routes/claim/question-page-options.js
+++ app/routes/claim/question-page-options.js
@@ -8,7 +8,6 @@ const questionPageOptions = {
         if (request.app['hapi-govuk-question-page']) {
           const dataToSet = request.app['hapi-govuk-question-page'].data
           if (dataToSet.mineType !== undefined) {
-            // ensure mineType is an array
             dataToSet.mineType = asArray(dataToSet.mineType)
           }
           sessionHandler.update(request, 'claim', dataToSet)
