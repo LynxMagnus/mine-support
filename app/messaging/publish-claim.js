e1958282e4e478d59aa0a1a0df28371e6b8db331
Fay Toward <fay.toward@rpa.gov.uk>
2021-04-07 11:26:57 +0100

    Protective monitoring
    Co-authored-by: steve-dickinson <steve-dickinson@users.noreply.github.com>

--- app/messaging/publish-claim.js
+++ app/messaging/publish-claim.js
@@ -2,6 +2,8 @@ const mqConfig = require('../config').claimQueueConfig
 const { MessageSender } = require('ffc-messaging')
 const createMessage = require('./create-message')
 const sessionHandler = require('../services/session-handler')
+const protectiveMonitoringService = require('../services/protective-monitoring-service')
+
 let claimSender
 
 async function stop () {
@@ -23,6 +25,7 @@ async function publishClaim (request) {
   const claim = sessionHandler.get(request, 'claim')
   const message = createMessage(claim)
   await claimSender.sendMessage(message)
+  protectiveMonitoringService.sendEvent(request, claim, 'Sending claim message')
   await claimSender.closeConnection()
 }
 
---
+++ app/services/protective-monitoring-service.js
@@ -0,0 +1,32 @@
+const { PublishEvent } = require('ffc-protective-monitoring')
+
+function sendEvent (request, claim, message) {
+  const protectiveMonitoring = new PublishEvent(process.env.PROTECTIVE_MONITORING_URL)
+  protectiveMonitoring.sendEvent({
+    sessionid: claim.claimId,
+    datetime: createEventDate(),
+    version: '1.1',
+    application: 'ffc-demo-web',
+    component: 'ffc-demo-web',
+    ip: getIpAddress(request),
+    pmccode: '001',
+    priority: '0',
+    details: {
+      message
+    }
+  })
+  console.log(`Protective monitoring message sent: ${message}`)
+}
+
+function getIpAddress (request) {
+  // Identifying the originating IP address of a client connecting to a web server through an HTTP proxy or a load balancer
+  const xForwardedForHeader = request.headers['x-forwarded-for']
+  return xForwardedForHeader ? xForwardedForHeader.split(',')[0] : request.info.remoteAddress
+}
+
+function createEventDate () {
+  const eventDate = new Date()
+  return eventDate.toISOString()
+}
+
+module.exports = { sendEvent }
