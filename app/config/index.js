0e70c8cff3fdfe18573bcc5ea6cc188a5e49f2c5
steve-dickinson <steve.dickinson@defra.gov.uk>
2021-04-09 07:11:18 +0100

    Change to joi validation

--- app/config/index.js
+++ app/config/index.js
@@ -25,7 +25,7 @@ const schema = Joi.object({
     clearInvalid: Joi.bool().default(false),
     strictHeader: Joi.bool().default(true)
   }),
-  protectiveMonitoringUrl: process.env.PROTECTIVE_MONITORING_URL
+  protectiveMonitoringUrl: Joi.string().allow('')
 })
 
 // Build config
